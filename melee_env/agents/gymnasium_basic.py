0afbb42411236576d553a8fb784b92fdf21e7add
will <will2346@live.co.uk>
2022-11-05 16:17:37 +0000

    randomfox agent (untested)

--- melee_env/agents/gymnasium_basic.py
+++ melee_env/agents/gymnasium_basic.py
@@ -15,3 +15,21 @@ class CPUFox:
 
     def act(self, gamestate):
         pass
+
+
+class RandomFox:
+    def __init__(self, press_start=False):
+        self.agent_type = "AI"
+        self.controller = None
+        self.port = None  # this is also in controller, maybe redundant?
+        self.action = 0
+        self.press_start = press_start
+        self.self_observation = None
+        self.current_frame = 0
+        self.character = enums.Character.FOX
+        self.action_space = action_space
+
+    def act(self, gamestate):
+        action = self.action_space.sample()
+        execute_action(action)
+        return action
